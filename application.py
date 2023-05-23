95c0c59ffb0b62b236abf31c3a6888e1f3db8e9a
tassalor1 <walkerconnor14@gmail.com>
2023-05-23 21:11:00 +0100

    Added application file

---
+++ application.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import pickle
+
+app = Flask(__name__, static_folder='C:\\Users\\Connor\\Desktop\\Coding\\nft_market_a\\Templates')
+
+@app.route('/')
+def home():
+    # Load the sentiment score from the pickle file
+    with open('sentiment_model.pkl', 'rb') as file:
+        sentiment = pickle.load(file)
+
+    # Get the overall sentiment score
+    overall_sentiment = overall_score(sentiment['compound'])
+
+    return render_template('front.html', sentiment=overall_sentiment)
+
+def overall_score(sentiment):
+    if sentiment >= 0.05:
+        return 'Positive'
+    elif sentiment <= -0.05:
+        return 'Negative'
+    else:
+        return 'Neutral'
+
+if __name__ == '__main__':
+    app.run()
