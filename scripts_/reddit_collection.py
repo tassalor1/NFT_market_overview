9d70ce44998fe3f35394bf20c727e70f35821d51
tassalor1 <walkerconnor14@gmail.com>
2023-06-19 23:32:08 +0100

    Creating script to collect NFT data

--- scripts_/reddit_collection.py
+++ scripts_/reddit_collection.py
@@ -1,13 +1,13 @@
 import os
-
+from dotenv import load_dotenv
 import praw
-
+load_dotenv()
 reddit = praw.Reddit(
-   client_id = os.getenv("REDDIT_CLIENT_ID"),
-   client_secret = os.getenv("REDDIT_SECRET"),
-   user_agent='tassalor1'
+   client_id=os.getenv("REDDIT_CLIENT_ID"),
+   client_secret=os.getenv("REDDIT_SECRET"),
+   user_agent='tassalor_sentiment'
 )
 
-hot_posts = reddit.subreddit('NFt').hot(limit=10)
+hot_posts = reddit.subreddit('NFT').hot(limit=10)
 for post in hot_posts:
     print(post.title)
