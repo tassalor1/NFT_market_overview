dd7a950f86490811a094d2668424a22380669b20
tassalor1 <walkerconnor14@gmail.com>
2023-06-21 00:06:43 +0100

    Better logic

--- scripts_/reddit/main_reddit.py
+++ scripts_/reddit/main_reddit.py
@@ -5,6 +5,7 @@ def run_script(script_path):
     os.system(f"python {script_path}")
 
 if __name__ == "__main__":
-    scripts = ["reddit_data_fetching.py", "reddit_data_preprocessor.py", "sentiment_analysis_model.py"]
+    scripts = ["reddit_data_fetching.py", "reddit_data_preprocessor.py", "reddit_model.py"]
     for script in scripts:
         run_script(script)
+    print("Script succesfully ran")
