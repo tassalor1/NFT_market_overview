f163ad10b0e282ef1586497002155d5bafc1f32e
tassalor1 <walkerconnor14@gmail.com>
2023-06-21 00:05:29 +0100

    Moved csv output

--- scripts_/reddit/reddit_data_fetching.py
+++ scripts_/reddit/reddit_data_fetching.py
@@ -24,7 +24,7 @@ def fetch_filtered_posts(reddit, unwanted_keywords):
                 results.append(("Comment", post.id, comment.body))
     return results
 
-def write_to_csv(filtered_posts_and_comments):
+def write_to_csv(filtered_posts_and_comments, file_path):
     with open('raw_reddit_nft_data.csv', 'w', newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
         writer.writerow(["Type", "Post ID", "Content"])
@@ -35,4 +35,4 @@ if __name__ == "__main__":
     reddit = load_enviroment()
     unwanted_keywords = ['giveaway', 'Like', 'RT', 'Giveaways', 'follow', 'free mint', 'trending', 'Sold', 'win']
     filtered_posts_and_comments  = fetch_filtered_posts(reddit, unwanted_keywords)
-    write_to_csv(filtered_posts_and_comments)
+    write_to_csv(filtered_posts_and_comments, 'C:\\Users\\Connor\\Desktop\\Coding\\nft_market_a\data\\raw_data')
