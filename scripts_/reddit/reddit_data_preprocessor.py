c0c18e3fa01a3c55beba2d4c15bec5c6a68b713e
tassalor1 <walkerconnor14@gmail.com>
2023-06-20 23:27:07 +0100

    Change layout for simpler organisation

---
+++ scripts_/reddit/reddit_data_preprocessor.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def process_text(reddit_content):
+    reddit_words = []
+    for word in reddit_content.split(' '):
+        if word.startswith('/u/') and len(word) > 3:
+            word = '/u/user'
+        elif word.startswith('http'):
+            word = "http"
+        reddit_words.append(word)
+    reddit_proc = " ".join(reddit_words)
+    return reddit_proc
+
+if __name__ == "__main__":
+    df = pd.read_csv("raw_reddit_nft_data.csv")
+    df['processed_text'] = df['Content'].apply(process_text)
+    df.to_csv('processed_reddit_nft_data.csv', index=False)
