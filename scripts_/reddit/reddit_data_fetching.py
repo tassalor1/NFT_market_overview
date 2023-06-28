de10a0daca2aadc906cc70dce1c01606f577b9de
tassalor1 <walkerconnor14@gmail.com>
2023-06-28 21:17:46 +0100

    Cleaning code and adding comments

--- scripts_/reddit/reddit_data_fetching.py
+++ scripts_/reddit/reddit_data_fetching.py
@@ -3,6 +3,8 @@ import csv
 from dotenv import load_dotenv
 import praw
 
+
+# Load reddit api keys
 def load_enviroment():
     load_dotenv()
     reddit = praw.Reddit(
@@ -12,18 +14,23 @@ def load_enviroment():
     )
     return reddit
 
+# Filter unwated words(spam/not NFT related)
 def fetch_filtered_posts(reddit, unwanted_keywords):
     results = []
     hot_posts = reddit.subreddit('all').search('nft', limit=10)
+
+    # For loop through api call
     for post in hot_posts:
+        # Checks for unwated words
         if not any(unwanted_keyword.lower() in post.title.lower() or unwanted_keyword.lower() in post.selftext.lower()
                 for unwanted_keyword in unwanted_keywords):
             results.append(("Post", post.id, post.title))
             post.comments.replace_more(limit=None)
-            for comment in post.comments.list()[1:]:
+            for comment in post.comments.list()[1:]: # First comment of most reddit posts are mod bots-so kip this comment
                 results.append(("Comment", post.id, comment.body))
     return results
 
+# Write to csv file
 def write_to_csv(filtered_posts_and_comments, file_path):
     with open('raw_reddit_nft_data.csv', 'w', newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
@@ -32,6 +39,7 @@ def write_to_csv(filtered_posts_and_comments, file_path):
             writer.writerow(item)
 
 if __name__ == "__main__":
+    # Call all functions
     reddit = load_enviroment()
     unwanted_keywords = ['giveaway', 'Like', 'RT', 'Giveaways', 'follow', 'free mint', 'trending', 'Sold', 'win']
     filtered_posts_and_comments  = fetch_filtered_posts(reddit, unwanted_keywords)
